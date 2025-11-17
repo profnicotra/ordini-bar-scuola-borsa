f11f69fae55032ef7f4e53d928e207350277e459
profnicotra <snicotra@scuola-borsa.it>
2025-11-17 14:42:33 +0100

    edit seed.sql

--- ordiniBarScuolaBorsa/schema.sql
+++ ordiniBarScuolaBorsa/schema.sql
@@ -6,6 +6,7 @@ CREATE TABLE IF NOT EXISTS prodotti (
   prezzo_interni NUMERIC(10,2)
   attivo BOOLEAN NOT NULL DEFAULT TRUE
 );
+
 CREATE TABLE IF NOT EXISTS posizioni (
   id SERIAL PRIMARY KEY,
   nome TEXT NOT NULL
@@ -29,6 +30,11 @@ CREATE TABLE IF NOT EXISTS  note (
   UNIQUE (id_gruppo, name),
 );
 
+CREATE TABLE IF NOT EXISTS stati (
+  id SERIAL PRIMARY KEY,
+  
+)
+
 CREATE TABLE IF NOT EXISTS ordini (
   id SERIAL PRIMARY KEY,
   posizione_id INTEGER NOT NULL REFERENCES posizioni(id),
@@ -49,7 +55,7 @@ CREATE TABLE IF NOT EXISTS ordine_righe_note (
   id SERIAL PRIMARY KEY,
   ordine_riga_id INTEGER NOT NULL REFERENCES ordini_riga(id) ON DELETE CASCADE,
   nota_id  INTEGER NOT NULL REFERENCES nota(id) ON DELETE CASCADE,
-)
+);
 
 CREATE TABLE IF NOT EXISTS impostazioni (
   chiave TEXT PRIMARY KEY,
@@ -58,21 +64,29 @@ CREATE TABLE IF NOT EXISTS impostazioni (
 CREATE INDEX IF NOT EXISTS idx_ordini_stato_creato ON ordini (stato, creato_il);
 
 --Query per verificare se bar è aperto
-SELECT valore from impostazioni where chiave = 'bar_aperto'
+SELECT valore from impostazioni where chiave = 'bar_aperto';
 
 --Query per aprire bar
-UPDATE impostazioni SET valore = 'true' where chiave = 'bar_aperto'
+UPDATE impostazioni SET valore = 'true' where chiave = 'bar_aperto';
 
 --Query per chiudere bar
-UPDATE impostazioni SET valore = 'false' where chiave = 'bar_aperto'
+UPDATE impostazioni SET valore = 'false' where chiave = 'bar_aperto';
+
+--Query per chiudere bar
+UPDATE impostazioni SET valore = 'false' where chiave = 'bar_aperto';
+
+--Query estrazione note generali
+SELECT n.nome nota
+FROM note n 
+WHERE id_gruèèp IS NULL;
 
 --Query estrazione prodotti
 SELECT p.nome prodotto, p.prezzo_euro, p.prezzo_interni, ng.esclusivo, ng.obbligatorio_default, n.nome nota,
 FROM prodotti p
 JOIN note_gruppi ng ON p.id = ng.id_prodotto
 JOIN note n ON p.id = ng.id_prodotto
-WHERE p.attivo = 1
-
+WHERE p.attivo = 1;
+  
 --query estrazione coda
 SELECT o.id, pr.nome prodotto, ori.quantita, n.nota nota, pos.nome posizione, o.stato, o.totale_euro
 FROM ordini o
@@ -80,4 +94,4 @@ JOIN ordine_righe ori ON o.id = ori.ordine_id
 JOIN ordine_righe_note orn ON ori.id = orn.ordine_riga_id
 JOIN note n ON orn.nota_id = n.id
 JOIN posizioni pos ON pos.id = o.posizione_id
-JOIN prodotti pr ON pr.id = ori.prodotto_id
+JOIN prodotti pr ON pr.id = ori.prodotto_id;
