06dbdf1f46fbef31ebc763124471e7642d944fc7
nick1-8 <nzapponi@gmail.com>
2025-11-20 10:55:12 +0100

    modifiche backend iniziali con dati fittizzi

--- ordiniBarScuolaBorsa/admin.py
+++ ordiniBarScuolaBorsa/admin.py
@@ -1,5 +1,23 @@
 from ordiniBarScuolaBorsa import app,render_template
+import json
+
 
 @app.route('/admin')
 def admin():
-    return render_template('admin.html')
+    
+
+    with open("C:\\Users\\24-info-22\\Desktop\\ANNO 25-26\\Linguaggi di programmazione\\ordini-bar-scuola-borsa\\ordiniBarScuolaBorsa\\products.json", "r") as products:
+        data = json.load(products)
+
+    data.update("id:3")
+    
+    with open ("C:\\Users\\24-info-22\\Desktop\\ANNO 25-26\\Linguaggi di programmazione\\ordini-bar-scuola-borsa\\ordiniBarScuolaBorsa\\products.json", "w"):
+        json.dump(data)
+        
+
+    print(data)
+
+    return render_template("admin.html")
+#     with open("C:\\Users\\24-info-22\\Desktop\\ANNO 25-26\\Linguaggi di programmazione\\ordini-bar-scuola-borsa\\ordiniBarScuolaBorsa\\products.json", "w") as products:
+#         data.append(newProduct)
+#         json.dump(data, products)
--- ordiniBarScuolaBorsa/orders.py
+++ ordiniBarScuolaBorsa/orders.py
@@ -1,5 +1,10 @@
 from ordiniBarScuolaBorsa import app,render_template
+import json
 
 @app.route('/orders')
 def orders():
-    return render_template('orders.html')
+    with open("C:\\Users\\24-info-22\\Desktop\\ANNO 25-26\\Linguaggi di programmazione\\ordini-bar-scuola-borsa\\ordiniBarScuolaBorsa\\products.json", "r") as productsJSON:
+        data = json.load(productsJSON)
+    i = len(data["products"])
+
+    return render_template('orders.html', products = data["products"], prices = data["prices"], available = data["available"], nProducts = i)
