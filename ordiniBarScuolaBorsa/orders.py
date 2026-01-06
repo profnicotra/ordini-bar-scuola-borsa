09dd7e9120a8112d91244b8622a802d43275ee0c
nick1-8 <nzapponi@scuola-borsa.it>
2026-01-06 15:23:36 +0100

    Aggiunti i nuovi dati a orders.py

--- ordiniBarScuolaBorsa/orders.py
+++ ordiniBarScuolaBorsa/orders.py
@@ -1,14 +1,9 @@
 from flask import Blueprint, render_template
+from ordiniBarScuolaBorsa import models
 
 bp = Blueprint("orders", __name__, url_prefix="/orders")
 
 @bp.get("/")
 def orders():
-    # with open("C:\\Users\\24-info-22\\Desktop\\ANNO 25-26\\Linguaggi di programmazione\\ordini-bar-scuola-borsa\\ordiniBarScuolaBorsa\\products.json", "r") as productsJSON:
-    #     data = json.load(productsJSON)
-    # i = len(data["products"])
-    data = {"title" : "Ordini Bar Scuola Borsa",
-        "open": True,   
-        "items" : []}
-    return render_template('orders.html')
-    #return render_template('orders.html', products = data["products"], prices = data["prices"], available = data["available"], nProducts = i)
+    data = models.get_products()
+    return render_template('orders.html', product = data)
