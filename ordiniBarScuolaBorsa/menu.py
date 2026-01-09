aa385da5467b418596b9c0649f50bff5d94e0d37
cpettignano-cmd <cpettignano@scuola-borsa.it>
2026-01-09 11:06:02 +0100

    lettura da file di dati

--- ordiniBarScuolaBorsa/menu.py
+++ ordiniBarScuolaBorsa/menu.py
@@ -1,5 +1,9 @@
 from ordiniBarScuolaBorsa import app,render_template
+import json
 
 @app.route('/menu')
 def menu():
+    with open ("./menu.json","r") as data:
+        productMenu=json.read(data)
+    print(productMenu)
     return render_template('menu.html')
