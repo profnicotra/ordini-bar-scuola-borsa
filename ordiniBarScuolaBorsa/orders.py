2aa6df2cb2f00b08c57ac87fadab7c79ad66568a
nick1-8 <nzapponi@scuola-borsa.it>
2026-01-18 00:39:31 +0100

    Modificata la pagina orders. I dati dell'utente e i prodotti che desidera vengono salvati nelle variabili python

--- ordiniBarScuolaBorsa/orders.py
+++ ordiniBarScuolaBorsa/orders.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect
 from ordiniBarScuolaBorsa import models
 
 bp = Blueprint("orders", __name__, url_prefix="/orders")
@@ -9,3 +9,15 @@ def orders():
         "open": True,   
         "items" : [models.get_products()]}
     return render_template('orders.html', data = data)
+
+@bp.route("/new_order", methods=["POST"])
+def new_order():
+    selectedProducts = request.form.get("prodottiSelezionati")
+    generalNote = request.form.get("noteGenerali")
+    customer_name = request.form.get("nome")
+    custumer_surname = request.form.get("cognome")
+    position = request.form.get("classe")
+    
+    print (f"Lista prodotti {selectedProducts}\nNote generale: {generalNote}\nNome cliente: {customer_name}\nCognome cliente: {custumer_surname}\nPosizione {position}")
+
+    return redirect("/orders")
